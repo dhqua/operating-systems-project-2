6fdd3b99e3c07673b291fa6a40b24bb396b4e4e7
cgnicolas <cgnicolas1@gmail.com>
2019-02-27 21:12:13 -0500

    Added LinkedList.h

---
+++ LinkedList.h
@@ -0,0 +1,41 @@
+#IFNDEF "LINKEDLIST.H"
+#define "LINKEDLIST.H"
+
+
+typedef struct LinkedList{
+    
+    struct Node* head;
+    struct Node* tail;
+
+} LinkedList;
+
+
+typedef struct Node {
+
+    struct Node* next;
+    unsigned pageNumber;
+} Node;
+
+void enqueue(LinkedList* linkedList, unsigned pageNum);
+
+
+unsigned dequeue(LinkedList* linkedList);
+
+LinkedList* create();
+
+void printList(Node* current);
+
+#endif
+/*
+int main(int argc, char* argv[]){
+    
+    LinkedList* linkedList = create();
+    enqueue(linkedList, 11111);
+    enqueue(linkedList, 22222);
+    enqueue(linkedList, 33333);
+    enqueue(linkedList, 44444);
+    printList(linkedList->head);
+    dequeue(linkedList);
+    printList(linkedList->head);
+    return 0;
+}*/
