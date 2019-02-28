08d9128124ec614f039a4bafb2c2d4fb9e7b48db
dhqua <quaon.thomas1@gmail.com>
2019-02-27 21:06:04 -0500

    Starting structure of the FIFO replacement policy

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include "linkedList.c"
+
 
 // Data structure to represent mem accesses read from file
     struct memReference 
@@ -10,19 +12,28 @@
 int main(int argc, char const *argv[])
 {
 
-    // int loopCount = 1005001;
+     int loopCount = 1005001;
     // int loopCount = 1005000;
-    int loopCount = 20;
+    //int loopCount = 20;
     int frameNumber = atoi(argv[2]);
     int traceCount = 0;
+    
+    // Simulated Structures
+    // Trace File
     struct memReference traceRead[loopCount];
+    // RAM
     struct memReference simRAM[frameNumber];
+    // Page Table
+    // 32 bit address / 4kb page size = 2^20 pages = 1048576
+    unsigned pageTable[1048576];
+
     printf("Size of struct: %lx\n", sizeof(struct memReference));
 
     // Capture policy and mode from command arguments
     const char* policy = argv[3];
     const char* mode = argv[4];
 
+    // Reads the contents of the memtrace file into our memtrace structure
     FILE *fptr = fopen(argv[1], "r");
     unsigned addr;
     char rw;
@@ -30,7 +41,8 @@ int main(int argc, char const *argv[])
     {
         traceRead[traceCount].memAddress = addr;
         traceRead[traceCount].accessType = rw;
-        printf("%d: Address: %x Access Type: %c\n",traceCount,traceRead[traceCount].memAddress,traceRead[traceCount].accessType);
+        if( mode == "debug")
+            printf("%d: Address: %x Access Type: %c\n",traceCount,traceRead[traceCount].memAddress,traceRead[traceCount].accessType);
         traceCount++;
     }
         
@@ -42,4 +54,25 @@ int main(int argc, char const *argv[])
 
     fclose(fptr);
     return 0;
+}
+
+// Function that returns the page number that should be replaced in RAM
+// checkAddress - cna
+unsigned fifoPolicy(LinkedList &visitedQueue, struct memReference &simulatedRam)
+{
+    unsigned addressToReplace = dequeue(visitedQueue); 
+    // Right shifts the adress by 12, in order to get the first 5 chars of the length 8 hex adress
+    // In other words this gets the page number by getting the first 20 bits of the 32 bit address
+    unsigned pageNumber = addressToReplace >> 12;    
+    
+    // Iterate through the RAM and find the position of the pageNumber within RAM
+    int ramPageNum = 0;
+    int ramLength = sizeof(simulatedRam) / sizeof(struct memReference);
+    for( : ramPageNum < ramLength, ramPageNum++)
+    {
+        // If the current contents of the this location in the ram matches the pagenumber that needs to be replaced
+        // then return the current location within the RAM
+        if( simulatedRam[ramPageNum] == pageNumber)
+            return ramPageNum;
+    }
 }
